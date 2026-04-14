f0b94e2116d2b95dc8aadb097500a130b38d18ec
StenReins <stenr1@tlu.ee>
2026-04-14 21:40:49 +0300

    added icons for nav butttons.

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+function navStatistics() {
+    window.location.href = '/pages/statistics.html'
+}
+
+function navSettings() {
+    window.location.href = '/pages/statistics.html'
+}
+
+function navBookAdd() {
+    window.location.href='/pages/bookAdd.html'
+}
